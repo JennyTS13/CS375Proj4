b25cbcb3e05b94768031c407918698494310d2f7
Tiffany Lam <tclam05@gmail.com>
2017-04-17 18:44:40 -0400

    driver and timer

--- src/Driver.java
+++ src/Driver.java
@@ -5,10 +5,30 @@
  * Project: 4
  */
 
+
 /**
  * A driver for testing different algorithms of multiplying two matrices.
  */
 public class Driver {
+
+    /**
+     * Tests a SubsetSum, finding the time and speed ups
+     * @param version the name of the SubsetSum being tested
+     * @param s the SubsetSum
+     */
+    private static void test(String version, SubsetSum s) {
+
+        //
+        Timer.start();
+
+        Timer.stop();
+
+        // output the results
+        System.out.println("--------" + version + "----------");
+
+        // output the time needed to find the product
+        System.out.println("Time: " + Timer.getRuntime() + "ms");
+    }
 
     public static void main(String[] args){
 
---
+++ src/Timer.java
@@ -0,0 +1,41 @@
+/*
+ * File:    Timer.java
+ * Authors: Charlie Beck, Phoebe Hughes, Tiffany Lam, Jenny Lin
+ * Date:    April 21, 2017
+ * Project: 4
+ * Copied from PrimeFinder
+ */
+
+/**
+ * Timer util class
+ */
+public class Timer {
+
+    /** The start time. */
+    private static long startTime;
+
+    /** The end time. */
+    private static long endTime;
+
+    /**
+     * Starts the timer.
+     */
+    public static void start() {
+        startTime = System.nanoTime();
+    }
+
+    /**
+     * Stops the timer.
+     */
+    public static void stop() {
+        endTime = System.nanoTime();
+    }
+
+    /**
+     * Calculates the runtime.
+     * @return the milliseconds between the last call of start and stop
+     */
+    public static long getRuntime() {
+        return (endTime - startTime) / 1000000;
+    }
+}
