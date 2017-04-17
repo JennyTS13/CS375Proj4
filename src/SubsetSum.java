cf3b8d78d6b460f8d393a826983158b8b634e51b
Jenny Lin <Jenny13TS13@gmail.com>
2017-04-17 18:46:15 -0400

    Adding SUbsetSum interface

---
+++ src/SubsetSum.java
@@ -0,0 +1,25 @@
+/*
+ * File:    SubsetSum.java
+ * Authors: Charlie Beck, Phoebe Hughes, Tiffany Lam, Jenny Lin
+ * Date:    April 21, 2017
+ * Project: 4
+ */
+
+import java.util.List;
+
+/**
+ * An interface used for the Subset Sum problem
+ */
+public interface SubsetSum {
+
+    /**
+     * Returns boolean indicating if a there exist a subset of the set S where
+     * the sum of its elements is equal to a specified sum k.
+     *
+     * @param multiset List of intergers in the multiset S
+     * @param sum int indicating the specified sum k
+     *
+     * @return boolean indicating if such a subset exists
+     */
+    boolean subsetExists(List<Integer> multiset, int sum);
+}
