e8c55c97ecde7ef25dd0fc987dfd7b4ce3c1d4ff
Jenny Lin <Jenny13TS13@gmail.com>
2017-04-17 20:44:48 -0400

    Adding SubsetSum Greedy

--- src/SubsetSumGreedy.java
+++ src/SubsetSumGreedy.java
@@ -5,10 +5,26 @@
  * Project: 4
  */
 
+import java.util.List;
+
 /**
  * Finds if there exists a subset of the set S where
  * the sum of its elements is equal to a specified sum k.
  * Greedy approach.
  */
-public class SubsetSumGreedy extends SubsetSum {
+public class SubsetSumGreedy implements SubsetSumApprox {
+
+    /**
+     * Returns the residue, the absolute value of the difference between
+     * the sum of the subset and the desired sum.
+     *
+     * @param multiset List of integers in the multiset S
+     * @param sum int indicating the specified sum k
+     *
+     * @return double - the residue
+     */
+    public double subsetResidue(List<Integer> multiset, int sum){
+        return 0;
+    }
+
 }
