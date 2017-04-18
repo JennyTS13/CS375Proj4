ed66775bcfb91e55f834bca61611beb7bbd1a914
Jenny Lin <Jenny13TS13@gmail.com>
2017-04-17 20:21:59 -0400

    Adding SubsetSum Greedy

--- src/SubsetSum.java
+++ src/SubsetSum.java
@@ -22,4 +22,15 @@ public interface SubsetSum {
      * @return boolean indicating if such a subset exists
      */
     boolean subsetExists(List<Integer> multiset, int sum);
+
+    /**
+     * Returns the residue, the absolute value of the difference between
+     * the sum of the subset and the desired sum.
+     *
+     * @param multiset List of integers in the multiset S
+     * @param sum int indicating the specified sum k
+     *
+     * @return double - the residue
+     */
+    double subsetResidue(List<Integer> multiset, int sum);
 }
---
+++ src/SubsetSumGreedy.java
@@ -0,0 +1,14 @@
+/*
+ * File:    SubsetSumExhaustive.java
+ * Authors: Charlie Beck, Phoebe Hughes, Tiffany Lam, Jenny Lin
+ * Date:    April 21, 2017
+ * Project: 4
+ */
+
+/**
+ * Finds if there exists a subset of the set S where
+ * the sum of its elements is equal to a specified sum k.
+ * Greedy approach.
+ */
+public class SubsetSumGreedy {
+}
