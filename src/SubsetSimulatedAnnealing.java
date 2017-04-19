0054f0556710a87415cab1d7b4a4ccff43c128aa
plhughes <phoebelhughes@gmail.com>
2017-04-18 21:46:53 -0400

    implemented subset stimulated annealing

--- src/SubsetSimulatedAnnealing.java
+++ src/SubsetSimulatedAnnealing.java
@@ -8,6 +8,16 @@ import java.util.Random;
  */
 public class SubsetSimulatedAnnealing implements SubsetSumApprox {
     private Random r = new Random();
+    private int numRepetitions;
+
+    public SubsetSimulatedAnnealing(int numRepetitions){
+        this.numRepetitions = numRepetitions;
+    }
+
+    public void setNumRepetitions(int numRepetitions){
+        this.numRepetitions = numRepetitions;
+    }
+
 
     public List<Integer> getNeighbor(List<Integer> originalSet, List<Integer> s){
         List<Integer> t = new ArrayList<>();
@@ -28,12 +38,12 @@ public class SubsetSimulatedAnnealing implements SubsetSumApprox {
 
         if(s.contains(originalSet.get(j))){
             if (r.nextInt(100)<50){
-                t.remove(originalSet.remove(j));
+                t.remove(originalSet.get(j));
             }
         }
         else{
             if (r.nextInt(100)<50){
-                t.add(originalSet.remove(j));
+                t.add(originalSet.get(j));
             }
         }
 
@@ -41,15 +51,63 @@ public class SubsetSimulatedAnnealing implements SubsetSumApprox {
 
     }
 
-    public int subsetResidue(List<Integer> multiset, int sum){
+    public List<Integer> getRandomSubset(List<Integer> multiset){
+        List<Integer> subset = new ArrayList<>();
+        for (Integer item: multiset){
+            if (r.nextInt(100) < r.nextInt(100)){
+                subset.add(item);
+            }
+        }
 
-        return 0;
+        return subset;
+    }
+
+    public int getSum(List<Integer> subset){
+        int sum = 0;
+        for (Integer i : subset){
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int subsetResidue(List<Integer> multiset, int sum){
+        List<Integer> randomSubset = this.getRandomSubset(multiset);
+        System.out.println(randomSubset);
+        int smallestResidue = -1;
+        for (int i = 0; i< this.numRepetitions; i++) {
+            List<Integer> neighbor = this.getNeighbor(multiset, randomSubset);
+            int neighborResidue = Math.abs(this.getSum(neighbor) - sum);
+            int residue = Math.abs(this.getSum(randomSubset) - sum);
+            if (neighborResidue >= residue){  //TODO: 10000000000 is too large an int... ask Dale
+                double T = (neighborResidue - residue)/ (1000000000 * Math.pow(0.8,(i/300)));
+                if ( r.nextDouble() > Math.pow(Math.E, -T)){
+                    randomSubset = neighbor;
+                }
+            }
+            else{ // neighborResidue < residue
+                randomSubset = neighbor;
+            }
+
+            if (smallestResidue < 0 ){
+                if (neighborResidue < residue){
+                    smallestResidue = neighborResidue;
+                }
+                else{
+                    smallestResidue = residue;
+                }
+            }
+            else if (neighborResidue < smallestResidue){
+                smallestResidue = neighborResidue;
+            }
+        }
+
+        return smallestResidue;
     }
 
     public static void main(String[] args){
-        SubsetSimulatedAnnealing subsetSimulatedAnnealing = new SubsetSimulatedAnnealing();
-        List<Integer> S = Arrays.asList(1, 3, 9, 2);
-        int residue = subsetSimulatedAnnealing.subsetResidue(S, 5);
+        SubsetSimulatedAnnealing subsetSimulatedAnnealing = new SubsetSimulatedAnnealing(100);
+        List<Integer> S = Arrays.asList(1, 3, 9, 2, 7, 34, 8, 2, 45, 4);
+        int residue = subsetSimulatedAnnealing.subsetResidue(S, 11);
         System.out.println("Residue: " + residue);
     }
 
